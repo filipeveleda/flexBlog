b561bae760095d25b0051bd5546204ecc4a91a57
Filipe V Mimbarcas <filipemimbarcas@gmail.com>
2021-02-12 12:51:28 -0300

    feat: animation-scroll

--- js/app.js
+++ js/app.js
@@ -18,3 +18,30 @@ function scrollSuave(){
 }
 
 scrollSuave();
+
+function iniAnimationScroll(){
+
+  const sections = document.querySelectorAll('.animation-scroll');
+
+  if (sections.length) {
+  const windowMetade = window.innerHeight * 0.60; 
+
+  function animaScroll(){
+    sections.forEach((section)=>{
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = (sectionTop - windowMetade) < 0;
+      
+      if (isSectionVisible) 
+        section.classList.add('ativo');
+      else
+        section.classList.remove('ativo');
+    });
+  }
+
+  animaScroll();
+  window.addEventListener('scroll',animaScroll);
+  }
+
+}
+
+iniAnimationScroll();
